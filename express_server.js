97e9eb6ad3cedd29a886ecc2d9c668a6d4d6fcbb
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-09-30 15:30:22 -0400

    URL Shortening (Part 1)

--- express_server.js
+++ express_server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs'); // templating engine
+
+const generateRandomString = function() {
+  return Math.random().toString(36).substr(2, 6);
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -29,3 +34,18 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => { // GET route to show the form
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send(generateRandomString());        // Respond with random 6-digit alphanumeric
+})
+
