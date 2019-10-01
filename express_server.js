939681988e6cc522048accba9ff9c2a9ef0b2704
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-10-01 12:42:34 -0400

    delete button added

--- express_server.js
+++ express_server.js
@@ -5,13 +5,13 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs'); // templating engine
 
-const generateRandomString = function() {
-  return Math.random().toString(36).substr(2, 6);
-};
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const generateRandomString = function() {
+  return Math.random().toString(36).substr(2, 6);
 };
 
 app.get("/", (req, res) => {
@@ -54,6 +54,12 @@ app.post("/urls", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  console.log(`ShortURL ${req.params.shortURL} has been deleted`);
+  res.redirect('/urls');
 });
 
 
