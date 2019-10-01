3a227d06051d23be9423f0058aebae258fb56ab4
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-10-01 15:56:26 -0400

    username login feature

--- express_server.js
+++ express_server.js
@@ -3,13 +3,14 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 app.set('view engine', 'ejs'); // templating engine
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
 const generateRandomString = function() {
   return Math.random().toString(36).substr(2, 6);
 };
@@ -31,16 +32,26 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => { // GET route to show the form
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -68,3 +79,13 @@ app.post("/urls/:id", (req, res) => {
   res.redirect('/urls');
 });
 
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  console.log(req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+})
